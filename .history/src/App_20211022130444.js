76d324232f479407e40c58e967391a7ed62f6dd0
mistertaavetti <alobalolu@gmail.com>
2022-01-08 13:42:08 +0100

    init

---
+++ .history/src/App_20211022130444.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState();
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  return <h2>tabs project setup</h2>;
+}
+
+export default App;
